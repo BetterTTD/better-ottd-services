9191f27df6667afcd5695ce046cdc8ce642a49b8
iTKerry <a.katrynets@gmail.com>
2021-12-04 12:43:47 +0200

    add enums with adminmessages

---
+++ src/OpenTTD/OpenTTD.Network/Enums/AdminUpdateType.cs
@@ -0,0 +1,16 @@
+﻿namespace OpenTTD.Network.Enums;
+
+public enum AdminUpdateType
+{
+	AdminUpdateDate,            /// Updates about the date of the game.
+	AdminUpdateClientInfo,     /// Updates about the information of clients.
+	AdminUpdateCompanyInfo,    /// Updates about the generic information of companies.
+	AdminUpdateCompanyEconomy, /// Updates about the economy of companies.
+	AdminUpdateCompanyStats,   /// Updates about the statistics of companies.
+	AdminUpdateChat,            /// The admin would like to have chat messages.
+	AdminUpdateConsole,         /// The admin would like to have console messages.
+	AdminUpdateCmdNames,       /// The admin would like a list of all DoCommand names.
+	AdminUpdateCmdLogging,     /// The admin would like to have DoCommand information.
+	AdminUpdateGamescript,      /// The admin would like to have gamescript messages.
+	AdminUpdateEnd,             /// Must ALWAYS be on the end of this list!! (period)
+}

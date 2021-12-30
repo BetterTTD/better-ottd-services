501e9c0f942b453a45b7e4321003d9526cb3f6ff
Alex <a.katrynets@gmail.com>
2021-12-30 03:05:37 +0200

    wip

---
+++ src/WebApps/Admin/Admin.Client/Pages/FetchDataViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Http.Json;
+using Admin.Shared;
+using Microsoft.AspNetCore.Components;
+
+namespace Admin.Client.Pages;
+
+public class FetchDataViewModel : BaseViewModel<WeatherForecast[]>
+{
+    [Inject]
+    private HttpClient Http { get; init; } = null!;
+
+    protected override async Task<WeatherForecast[]?> InitializeAsync()
+    {
+        return await Http.GetFromJsonAsync<WeatherForecast[]>("sample-data/weather.json");
+    }
+}
---
+++ src/WebApps/Admin/Admin.Shared/WeatherForecast.cs
@@ -0,0 +1,12 @@
+﻿namespace Admin.Shared;
+
+public record WeatherForecast
+{
+    public DateTime Date { get; init; }
+
+    public int TemperatureC { get; init; }
+
+    public string? Summary { get; init; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}

2d719e4109a470c6646d2c6215a45399f991f646
Alex <a.katrynets@gmail.com>
2022-01-05 16:58:27 +0200

    add seq and pino loggers

--- src/Services/OpenTTD/OpenTTD.AdminClient/clientService.js
+++ src/Services/OpenTTD/OpenTTD.AdminClient/clientService.js
@@ -4,16 +4,6 @@ const { UpdateFrequencies, UpdateTypes } = packetEnums;
 
 const rules = "Short server rules - No stealing, no trolling, no teleporting, no city grids.";
 
-let clients = [];
-
-function clientAuth(client, ip, port, name, pass) {
-    client.connect(ip, port);
-    client.on('connect', () => {
-        console.log(`[CONNECT]:`);
-        client.authenticate(name, pass);
-    });
-}
-
 function sendRcon(client, command) {
     client.send_rcon(command);
 }
@@ -22,90 +12,101 @@ function sayClient(client, clientId, text) {
     sendRcon(client, `say_client ${clientId} \"${text}\"`);
 }
 
-function clientObserve(client) {
-    client.on('welcome', (welcome) => {
-        console.log(`[WELCOME]: ${JSON.stringify(welcome)}`);
-        client.send_update_frequency(UpdateTypes.CHAT, UpdateFrequencies.AUTOMATIC);
-        client.send_update_frequency(UpdateTypes.CLIENT_INFO, UpdateFrequencies.AUTOMATIC);
-        //client.send_update_frequency(UpdateTypes.CONSOLE, UpdateFrequencies.AUTOMATIC);
-        client.send_poll(UpdateTypes.CLIENT_INFO, Number.MAX_SAFE_INTEGER);
-    });
+export default class ClientService {
+    clients = [];
 
-    client.on('chat', (chat) => {
-        console.log(`[CHAT]: ${JSON.stringify(chat)}`);
+    constructor(logger) {
+        this.logger = logger;
+    }
+    
+    Auth(client, ip, port, name, pass) {
+        client.connect(ip, port);
+        client.on('connect', () => {
+            console.log(`[CONNECT]:`);
+            client.authenticate(name, pass);
+        });
+    }
 
-        let sayCli = (message) => sayClient(client, chat.id, message);
-        let sendRc = (command) => sendRcon(client, command);
-
-        let cmdExact = (str) => chat.message === str;
-        let cmdStarts = (str) => chat.message.startsWith(str);
-
-        switch (true) {
-            case cmdExact('!rules'):
-                sayCli(rules);
-                break;
-            case cmdExact('!reset') || cmdExact('!resetme'):
-                let cli = clients.find(c => c.id === chat.id);
-                let companyResetCmd = `reset_company ${cli.company}`;
-                let moveClientCmd = `move ${cli.id} 255`;
-                sendRc(moveClientCmd);
-                sendRc(companyResetCmd);
-                break;
-            case cmdStarts('!rename ') ||cmdStarts('!name '):
-                let clientNameCmd = `client_name ${chat.id} \"${chat.message.replace('!rename ', '').replace('!name ', '')}\"`;
-                sendRc(clientNameCmd);
-                break;
-            default:
-                break;
-        }
-    });
-
-    client.on('clientinfo', (clientInfo) => {
-        console.log(`[CLIENT_INFO]: ${JSON.stringify(clientInfo)}`);
-        clients = [...clients.filter(c => c.id !== clientInfo.id), { ...clientInfo, company: clientInfo.company + 1 }];
-    });
-
-    client.on('error', function (error) {
-        console.log(`[ERROR]: ${JSON.stringify(error)}`);
-        clients = [...clients.filter(c => c.id !== error.id)];
-    });
-
-    client.on('clientjoin', function (clientJoin) {
-        console.log(`[CLIENTJOIN]: ${JSON.stringify(clientJoin)}`);
-
-        let sayCli = (message) => sayClient(client, clientJoin, message);
-        sayCli("Welcome to TG Vanilla server - reddit.com/r/openttd legacy.");
-        sayCli(rules);
-        sayCli("Server commands:");
-        sayCli("!rules");
-        sayCli("!rename or !name");
-        sayCli("!resetme or !reset");
-        sayCli("!info");
-    });
-
-    client.on('clientupdate', function (clientUpdate) {
-        console.log(`[CLIENTUPDATE]: ${JSON.stringify(clientUpdate)}`);
-        let cli = clients.find(c => c.id === clientUpdate.id);
-        let updatedProps = { name: clientUpdate.name, company: clientUpdate.company + 1 };
-        clients = [...clients.filter(c => c.id !== clientUpdate.id), { ...cli, ...updatedProps }];
-    });
-
-    client.on('clientquit', function (clientQuit) {
-        console.log(`[CLIENTQUIT]: ${JSON.stringify(clientQuit)}`);
-        clients = [...clients.filter(c => c.id !== clientQuit.id)];
-    });
-
-    client.on('clienterror', function (clientError) {
-        console.log(`[CLIENTERROR]: ${JSON.stringify(clientError)}`);
-        clients = [...clients.filter(c => c.id !== clientError.id)];
-    });
-
-    client.on('console', function (consol) {
-        console.log(`[CONSOLE]: ${JSON.stringify(consol)}`);
-    });
+    Observe(client) {
+        client.on('welcome', (welcome) => {
+            console.log(`[WELCOME]: ${JSON.stringify(welcome)}`);
+            client.send_update_frequency(UpdateTypes.CHAT, UpdateFrequencies.AUTOMATIC);
+            client.send_update_frequency(UpdateTypes.CLIENT_INFO, UpdateFrequencies.AUTOMATIC);
+            //client.send_update_frequency(UpdateTypes.CONSOLE, UpdateFrequencies.AUTOMATIC);
+            client.send_poll(UpdateTypes.CLIENT_INFO, Number.MAX_SAFE_INTEGER);
+        });
+    
+        client.on('chat', (chat) => {
+            console.log(`[CHAT]: ${JSON.stringify(chat)}`);
+    
+            let sayCli = (message) => sayClient(client, chat.id, message);
+            let sendRc = (command) => sendRcon(client, command);
+    
+            let cmdExact = (str) => chat.message === str;
+            let cmdStarts = (str) => chat.message.startsWith(str);
+    
+            switch (true) {
+                case cmdExact('!rules'):
+                    sayCli(rules);
+                    break;
+                case cmdExact('!reset') || cmdExact('!resetme'):
+                    let cli = this.clients.find(c => c.id === chat.id);
+                    let companyResetCmd = `reset_company ${cli.company}`;
+                    let moveClientCmd = `move ${cli.id} 255`;
+                    sendRc(moveClientCmd);
+                    sendRc(companyResetCmd);
+                    break;
+                case cmdStarts('!rename ') ||cmdStarts('!name '):
+                    let clientNameCmd = `client_name ${chat.id} \"${chat.message.replace('!rename ', '').replace('!name ', '')}\"`;
+                    sendRc(clientNameCmd);
+                    break;
+                default:
+                    break;
+            }
+        });
+    
+        client.on('clientinfo', (clientInfo) => {
+            console.log(`[CLIENT_INFO]: ${JSON.stringify(clientInfo)}`);
+            this.clients = [...this.clients.filter(c => c.id !== clientInfo.id), { ...clientInfo, company: clientInfo.company + 1 }];
+        });
+    
+        client.on('error', (error) => {
+            console.log(`[ERROR]: ${JSON.stringify(error)}`);
+            this.clients = [...this.clients.filter(c => c.id !== error.id)];
+        });
+    
+        client.on('clientjoin', function (clientJoin) {
+            console.log(`[CLIENTJOIN]: ${JSON.stringify(clientJoin)}`);
+    
+            let sayCli = (message) => sayClient(client, clientJoin, message);
+            sayCli("Welcome to TG Vanilla server - reddit.com/r/openttd legacy.");
+            sayCli(rules);
+            sayCli("Server commands:");
+            sayCli("!rules");
+            sayCli("!rename or !name");
+            sayCli("!resetme or !reset");
+            sayCli("!info");
+        });
+    
+        client.on('clientupdate', (clientUpdate) => {
+            console.log(`[CLIENTUPDATE]: ${JSON.stringify(clientUpdate)}`);
+            let cli = this.clients.find(c => c.id === clientUpdate.id);
+            let updatedProps = { name: clientUpdate.name, company: clientUpdate.company + 1 };
+            this.clients = [...this.clients.filter(c => c.id !== clientUpdate.id), { ...cli, ...updatedProps }];
+        });
+    
+        client.on('clientquit', (clientQuit) => {
+            console.log(`[CLIENTQUIT]: ${JSON.stringify(clientQuit)}`);
+            this.clients = [...this.clients.filter(c => c.id !== clientQuit.id)];
+        });
+    
+        client.on('clienterror', (clientError) => {
+            console.log(`[CLIENTERROR]: ${JSON.stringify(clientError)}`);
+            this.clients = [...this.clients.filter(c => c.id !== clientError.id)];
+        });
+    
+        client.on('console', (consol) => {
+            console.log(`[CONSOLE]: ${JSON.stringify(consol)}`);
+        });
+    }
 }
-
-export let clientService = {
-    auth: clientAuth,
-    observe: clientObserve
-};
--- src/Services/OpenTTD/OpenTTD.AdminClient/index.js
+++ src/Services/OpenTTD/OpenTTD.AdminClient/index.js
@@ -1,13 +1,37 @@
 import express from 'express';
 import { connection } from 'node-openttd-admin';
 import { v4 as uuidv4 } from 'uuid';
-import { clientService } from './clientService.js';
+import ClientService from './clientService.js';
+import pinoToSeq from 'pino-seq';
+import { pinoHttp } from 'pino-http';
+import pinoms from 'pino-multi-stream';
 
 // configure
 
+//const stream = pinoToSeq.createStream({ serverUrl: 'http://localhost:5341' });
+
+const logger = pinoms({
+    name: 'OpenTTD.AdminClient', 
+    streams: [
+        {level: 'debug', stream: process.stdout},
+        {level: 'error', stream: process.stderr},
+        {level: 'debug', stream: pinoToSeq.createStream({ serverUrl: 'http://tg.seq:5341' })}
+    ]
+});
+
+const clientService = new ClientService(logger);
 const port = 80;
 const app = express();
+
 app.use(express.json());
+app.use(pinoHttp({ 
+    logger: logger, 
+    wrapSerializers: true, 
+    useLevel: 'info',
+    customSuccessMessage: function (res) {
+        return `[${res.req.method}] ${res.req.url}`;
+    }
+}));
 
 // app state
 
@@ -24,11 +48,11 @@ app.get('/ping', (_, res) => {
 });
 
 app.get('/clients', (_, res) => {
-    let result = clients.map(client => { 
+    let result = clients.map(client => {
         return {
-            id: client.id, 
-            ip: client.ip, 
-            port: client.port 
+            id: client.id,
+            ip: client.ip,
+            port: client.port
         };
     });
     res.status(200).send(result);
@@ -44,8 +68,8 @@ app.post('/add-client', (req, res) => {
 
     let client = new connection();
 
-    clientService.auth(client, ip, port, botName, pass);
-    clientService.observe(client);
+    clientService.Auth(client, ip, port, botName, pass);
+    clientService.Observe(client);
 
     let clientInfo = { id: uuidv4(), ip: ip, port: port, conn: client };
     clients = [...clients, clientInfo];
@@ -53,4 +77,4 @@ app.post('/add-client', (req, res) => {
     res.status(200).send(clientInfo.id);
 });
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+app.listen(port);

b278d5b9d193bc2c0891a50c50cc3c9afd117225
Alex <a.katrynets@gmail.com>
2022-01-22 02:42:47 +0200

    add endpoints

--- src/Services/OpenTTD/OpenTTD.API/Program.fs
+++ src/Services/OpenTTD/OpenTTD.API/Program.fs
@@ -1,17 +1,67 @@
-open Microsoft.Extensions.Logging
+open System
+open Microsoft.FSharp.Core
 open Saturn
 open Giraffe
+
 open Microsoft.Extensions.Hosting
 open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Http
+open Microsoft.Extensions.Logging
 
-let endpointPipe = pipeline {
-    plug fetchSession
-    plug head
-    plug requestId
-}
+[<CLIMutable>]
+type ClientCreateRequest =
+    { Ip      : string
+      Port    : int
+      Pass    : string
+      BotName : string 
+      BotVer  : string }
+    
+[<CLIMutable>]
+type ClientUpdateRequest =
+    { Ip      : string
+      Port    : int
+      Pass    : string
+      BotName : string 
+      BotVer  : string }
+    
+let getClients : HttpHandler =
+    fun (next : HttpFunc) (ctx : HttpContext) ->
+        task {
+            return! json "" next ctx
+        }
+
+let getClient (id : Guid) : HttpHandler =
+    fun (next : HttpFunc) (ctx : HttpContext) ->
+        task {
+            return! json id next ctx
+        }
+
+let createClient : HttpHandler =
+    fun (next : HttpFunc) (ctx : HttpContext) ->
+        task {
+            let! request = ctx.BindJsonAsync<ClientCreateRequest>()
+            return! json request next ctx
+        }
+
+let updateClient (id : Guid) : HttpHandler =
+    fun (next : HttpFunc) (ctx : HttpContext) ->
+        task {
+            let! request = ctx.BindJsonAsync<ClientUpdateRequest>()
+            return! json request next ctx
+        }
+
+let deleteClient (id : Guid) : HttpHandler =
+    fun (next : HttpFunc) (ctx : HttpContext) ->
+        task {
+            return! json id next ctx
+        }
 
 let apiRouter = router {
-    get "/" (text "Hello World from Saturn")
+    get "/clients" getClients
+    getf "/clients/%O" getClient
+    put "/clients/create" createClient
+    putf "/clients/%O/update" updateClient
+    deletef "/clients/%O/delete" deleteClient
 }
 
 let topRouter = router {
@@ -37,13 +87,7 @@ let configureApplication app =
         app
 
 let app = application {
-    pipe_through endpointPipe
-    
-    memory_cache
-    
     use_router topRouter
-    use_gzip
-    
     logging configureLogging
     service_config configureServices
     app_config configureApplication

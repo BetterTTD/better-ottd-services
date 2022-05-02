e9e6d5d341a0c301c95e5762441c9de13d84bcb8
Alex <a.katrynets@gmail.com>
2022-05-03 01:46:49 +0300

    add api

--- src/WebApps/Admin/AdminApp/Server/Controllers/WeatherForecastController.cs
+++ src/WebApps/Admin/AdminApp/Server/Controllers/WeatherForecastController.cs
@@ -7,8 +7,7 @@ namespace AdminApp.Server.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
+        private static readonly string[] Summaries = {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 

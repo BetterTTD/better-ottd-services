a75a2ad2b018032ea0af6c9a5804963616985f12
iTKerry <a.katrynets@gmail.com>
2022-07-24 01:58:11 +0300

    wip

--- src/OpenTTD/OpenTTD.Actors/Server/Messages.cs
+++ src/OpenTTD/OpenTTD.Actors/Server/Messages.cs
@@ -1,4 +1,8 @@
-﻿namespace OpenTTD.Actors.Server;
+﻿using Domain.ValueObjects;
+
+namespace OpenTTD.Actors.Server;
 
 public sealed record Connect;
 public sealed record Disconnect;
+
+public sealed record ServerStateChanged(ServerId ServerId, State State);

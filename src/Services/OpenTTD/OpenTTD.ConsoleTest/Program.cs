c36c15fcea5018e8db8993350f112686b25a6a15
Oleksandr K <con-ok@danelec.com>
2024-02-29 12:43:07 +0200

    cleanup

--- src/Services/OpenTTD/OpenTTD.ConsoleTest/Program.cs
+++ src/Services/OpenTTD/OpenTTD.ConsoleTest/Program.cs
@@ -1,16 +1,9 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using OpenTTD.ConsoleTest;
-using StackExchange.Redis;
 
 var host = Host
     .CreateDefaultBuilder(args)
-    .ConfigureServices((_, services) => services
-        .AddSingleton<IConnectionMultiplexer>(sp =>
-        {
-            const string connectionString = "localhost:6379";
-            return ConnectionMultiplexer.Connect(connectionString);
-        })
-        .AddHostedService<App>());
+    .ConfigureServices((_, services) => services.AddHostedService<App>());
 
 host.RunConsoleAsync();
